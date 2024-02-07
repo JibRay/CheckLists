4b751451b1717a7646afe92e8babf697bff359ad
Jib Ray <jib2ray@gmail.com>
2024-02-07 12:47:32 -0800

    Initial Commit

---
+++ CheckLists/CheckListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckListsApp.swift
+//  CheckLists
+//
+//  Created by Jib Ray on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CheckListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
